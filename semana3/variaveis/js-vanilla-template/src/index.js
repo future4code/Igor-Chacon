8593b0c945bc270a4ab59b6b3ea535d5d35234be
Igor Chacon <igorchacon@yahoo.com.br>
2020-11-30 16:59:04 -0300

    quase pronto

--- semana3/variaveis/js-vanilla-template/src/index.js
+++ semana3/variaveis/js-vanilla-template/src/index.js
@@ -5,40 +5,81 @@
 // 2.
 // 10 10 10
 
-let nome
-let idade
-console.log(typeof nome, typeof idade)
+
+
+
+
+
+// 1.
+
+// let nome
+// let idade
+// console.log(typeof nome, typeof idade)
 
 /* O tipo impresos é undefined, pois as variáveis ainda não foram definidas em seu tipo,
 só serão quando houver alguma outra definição.*/
-nome = prompt("diga seu nome")
-idade = prompt("diga sua idade")
-console.log(nome)
-console.log(idade)
 
-console.log(typeof nome, typeof idade)
+
+// nome = prompt("diga seu nome")
+// idade = prompt("diga sua idade")
+// console.log(nome)
+// console.log(idade)
+
+// console.log(typeof nome, typeof idade)
 
 /* agora que as variáveis foram definidas, o js pode definir um tipo para elas. */
 
 
-console.log("Olá " + nome +", você tem " + idade + " anos")
+// console.log("Olá " + nome +", você tem " + idade + " anos")
 
 
-pergunta1 = prompt("1. Qual seu maior sonho?")
-pergunta2 = prompt("2. onde você quer estar daqui 10 anos?")
-pergunta3 = prompt("3. se voce fosse para uma ilha deserta, qual livro levaria?")
-pergunta4 = prompt("4. se fosse um animal, qual seria?")
-pergunta5 = prompt("5. se fosse um país, qual seria?")
 
-console.log("Olá " + nome +", você tem " + idade + " anos")
-console.log("1. Qual seu maior sonho?")
-console.log(pergunta1)
-console.log("2. onde você quer estar daqui 10 anos?")
-console.log(pergunta2)
-console.log("3. se voce fosse para uma ilha deserta, qual livro levaria?")
-console.log(pergunta3)
-console.log("4. se fosse um animal, qual seria?")
-console.log(pergunta4)
-console.log("5. se fosse um país, qual seria?")
-console.log(pergunta5)
+
+
+
+
+// 2.
+
+// pergunta1 = prompt("1. Qual seu maior sonho?")
+// pergunta2 = prompt("2. onde você quer estar daqui 10 anos?")
+// pergunta3 = prompt("3. se voce fosse para uma ilha deserta, qual livro levaria?")
+// pergunta4 = prompt("4. se fosse um animal, qual seria?")
+// pergunta5 = prompt("5. se fosse um país, qual seria?")
+
+// console.log("Olá " + nome +", você tem " + idade + " anos")
+// console.log("1. Qual seu maior sonho?")
+// console.log(pergunta1)
+// console.log("2. onde você quer estar daqui 10 anos?")
+// console.log(pergunta2)
+// console.log("3. se voce fosse para uma ilha deserta, qual livro levaria?")
+// console.log(pergunta3)
+// console.log("4. se fosse um animal, qual seria?")
+// console.log(pergunta4)
+// console.log("5. se fosse um país, qual seria?")
+// console.log(pergunta5)
+
+// 3.
+
+// let comidas = ["ravioli", "macarronada", "pudim", "bolo da moça", "baião de dois"]
+// console.log(comidas)
+// console.log("Essas são minhas comidas preferidas: ")
+// console.log(comidas[0])
+// console.log(comidas[1])
+// console.log(comidas[2])
+// console.log(comidas[3])
+// console.log(comidas[4])
+
+// comidas[1] = prompt("qual sua comida preferida?")
+// console.log("Essas são minhas comidas preferidas (com a sua na segunda prosição): ")
+// console.log(comidas[0])
+// console.log(comidas[1])
+// console.log(comidas[2])
+// console.log(comidas[3])
+// console.log(comidas[4])
+
+
+
+
+
+// 4.
 

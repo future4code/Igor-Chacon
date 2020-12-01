b2ceca520f90343c74f8fcb90a1a2b7b69c902c1
Igor Chacon <igorchacon@yahoo.com.br>
2020-12-01 14:22:05 -0300

    tarefa pronta (faltam desafios)

---
+++ semana3/operadores/js-vanilla-template/src/index.js
@@ -0,0 +1,47 @@
+// 1.
+// a. false
+// b. false
+// c. true,
+// e. boolean
+
+// 2.
+// a. undefined
+// b. null
+// c. 11
+// d. 3
+// e. [3, 19, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+// f. 9
+
+
+// 1.
+// idadeUsuario = prompt("Qual a sua idade?")
+// idadeAmigo = prompt("Qual idade do seu melhor amigo?")
+
+// resultado1 = idadeUsuario > idadeAmigo
+
+// console.log("Sua idade é maior do que a do seu melhor amigo? "+resultado1)
+// console.log(idadeUsuario - idadeAmigo)
+
+
+// 2.
+// numero = prompt("Insira um número par.")
+// console.log(numero%2)
+// o resultado é sempre zero para números pares.
+// se o número digitado for impar, o resto é 1
+
+
+// 3.
+// listaDeTarefas = []
+// listaDeTarefas[0] = prompt("digite uma tarefa pra realizar no dia de hoje")
+// listaDeTarefas[1] = prompt("digite uma tarefa pra realizar no dia de hoje")
+// listaDeTarefas[2] = prompt("digite uma tarefa pra realizar no dia de hoje")
+// console.log(listaDeTarefas)
+// i = prompt("digite um índice já utilizado (0, 1, 2)")
+// listaDeTarefas.splice(i, 1)
+// console.log(listaDeTarefas)
+
+
+// 4.
+// nomeUsuario = prompt("digite seu nome.")
+// emailUsuario = prompt("digite seu e-mail.")
+// console.log("O e-mail " + emailUsuario + " foi cadastrado com sucesso. Seja bem-vindo(a), " +  nomeUsuario + ".")

39a91538bfb620032074639f40789d8b4eb1440f
Igor Chacon <igorchacon@yahoo.com.br>
2020-12-02 13:46:46 -0300

    Tudo feito

---
+++ semana3/condicionais/js-vanilla-template/src/index.js
@@ -0,0 +1,94 @@
+// Exercício 1
+/* O código pede que o usuário digite um número (prompt).
+Em seguida pega o número (que era uma string) e transforma em number.
+Com isso faz uma condicional onde caso o resto da divisão do número for 0
+ele imprime que passou no testo, caso não, imprime que não passou no teste.
+Na prática, o código diz se o número é par ou impar.
+*/
+
+/* Exercício 2 
+a. O código seleciona uma das opções dentro do laço "Switch" dependendo da entrada (fruta).
+Em seguida dá a variável "preço" o valor correspondente do caso do switch escolhido.
+por fim, imprime a frase com a fruta escolhida e o preço.
+
+b. 
+O preço da fruta Maçã é R$ 2.25
+
+c. retirando o break desse case, o código roda o que está no defaut caso se escolha Pêra 
+*/
+
+/* Exercício 3
+a. A primeira linha do código está pedindo para que o usuário escreva um número e
+o transformando em number.
+
+b. se o número digitado for 10 a mensagem será:
+Esse número passou no teste
+
+se o número for -10 não haverá mensagem de dentro do if.
+
+c. como a mensagem está declarada como let, ela só pode ser acessada
+de dentro do contexto do if.
+*/
+
+
+/* Exercício 4*/
+
+// idadeUsuario = Number(prompt("Escreva sua idade."))
+// if (idadeUsuario != false) {
+//     if (idadeUsuario >= 18) {
+//         console.log("Você pode dirigir.")
+//     }else {
+//         console.log("Você não pode dirigir.")
+//     }
+// }else{
+//     console.log("Você não digitou um número. Fim")
+// }
+
+
+/* Exercício 5 */
+
+// turdoAluno = prompt("Em qual turno você estuda M (matutino) ou V (Vespertino) ou N (Noturno)?")
+
+// if (turdoAluno === "M") {
+//     console.log("Bom dia!")
+// }else if (turdoAluno === "V") {
+//     console.log("Boa Tarde!")
+// }else if (turdoAluno === "N") {
+//     console.log("Boa Noite!")
+// }else if (turdoAluno != "M" || turdoAluno != "V" || turdoAluno != "N") {
+//     console.log("Entrada inválida.")
+// }
+
+/* Exercício 6 */
+
+// let turmaAluno = prompt("Em qual turno você estuda M (matutino) ou V (Vespertino) ou N (Noturno)?")
+// let mensagemBoasVindas
+// switch (turmaAluno) {
+//     case "M":
+//         mensagemBoasVindas = "Bom dia!"
+//         break;
+//     case "V":
+//         mensagemBoasVindas = "Boa tarde!"
+//         break;
+//     case "N":
+//         mensagemBoasVindas = "Boa Noite!"
+//         break;
+//     default:
+//         mensagemBoasVindas = "Entrada inválida"
+//         break;
+// }
+// console.log(mensagemBoasVindas)
+
+/* Exercício 6 */
+
+// let generoFilme = prompt("Digite o gênero do filme.")
+// let precoIngreco = Number(prompt("Digite o preço do ingreço."))
+
+// if (generoFilme === "fantasia" && precoIngreco < 15) {
+//     console.log("Bom filme!")
+// } else {
+//     console.log("Escolha outro filme :(")
+// }
+
+/* Desafios */
+

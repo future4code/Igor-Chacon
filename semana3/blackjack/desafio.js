bce2382ec43a949ca7c63ef3a640cb264b2bbcf7
Igor Chacon <igorchacon@yahoo.com.br>
2020-12-04 14:27:13 -0300

    corrigido no arquivo certo

--- semana3/blackjack/desafio.js
+++ semana3/blackjack/desafio.js
@@ -10,28 +10,3 @@
  * 
  * 
  */
-alert("Bem vindo ao jogo de Blackjack!")
-if(confirm("Quer iniciar uma nova rodada?")) {
-   console.log("Vamos lá")
-   const carta1Jogador = comprarCarta()
-   const carta2Jogador = comprarCarta()
-   const carta1Computador = comprarCarta()
-   const carta2Computador = comprarCarta()
-   console.log("Cartas do jogador: "+carta1Jogador.texto, carta2Jogador.texto)
-   console.log("Pontuação do jogador: " + (carta1Jogador.valor + carta2Jogador.valor))
-   console.log("Cartas do computador: "+carta1Computador.texto, carta2Computador.texto)
-   console.log("Pontuação do jogador: " + (carta1Computador.valor + carta2Computador.valor))
-
-   if ((carta1Jogador.valor + carta2Jogador.valor) > (carta1Computador.valor + carta2Computador.valor)) {
-      console.log("Jogador ganha a rodada")
-   } else if ((carta1Jogador.valor + carta2Jogador.valor) < (carta1Computador.valor + carta2Computador.valor)) {
-      console.log("Computador ganha a rodada")
-   } else if ((carta1Jogador.valor + carta2Jogador.valor) === (carta1Computador.valor + carta2Computador.valor)) {
-      console.log("A rodada empatou")
-   }
-
-
-} else {
-   console.log("O jogo acabou.")
-	// o que fazer se o usuário clicar "cancelar"
-}

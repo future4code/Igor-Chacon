4fd1ccb449c925842b180b05e43c9626dbb9348a
Igor Chacon <igorchacon@yahoo.com.br>
2020-12-09 17:27:31 -0300

    feitos desafios

--- semana4/objetos/index.js
+++ semana4/objetos/index.js
@@ -29,17 +29,53 @@ document.getElementById("botaoPostar").onclick = function postar() {
     areaPost = document.getElementById("container-de-posts")
 
     if (objetoPostagem.imgPostagem != "") {
-        areaPost.innerHTML += `<div class="postagemBlog"><id=\"${objetoPostagem.idPostagem}\"> 
-                                <div id="tituloTexto"> <h1>${objetoPostagem.tituloPostagem}</h1> 
-                                <img src="${objetoPostagem.imgPostagem}" alt="imagemPost${objetoPostagem.idPostagem}"> 
-                                </div> <div id="autorTexto"> Por   ${objetoPostagem.autorPostagem} <br> </div> 
-                                <div id="textoTexto"> <br> ${objetoPostagem.textoPostagem} </div></div>`
-    } else if (objetoPostagem.imgPostagem == "") {
-        areaPost.innerHTML += `<div class="postagemBlog"><id=\"${objetoPostagem.idPostagem}\"> 
-                                <div id="tituloTexto"> <h1>${objetoPostagem.tituloPostagem}</h1>
-                                </div> <div id="autorTexto"> Por   ${objetoPostagem.autorPostagem} <br> </div> 
-                                <div id="textoTexto"> <br> ${objetoPostagem.textoPostagem} </div></div>`
+        let opened = window.open();
+        opened.document.write(`<!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <meta http-equiv="X-UA-Compatible" content="ie=edge">
+            <script src="index.js" defer></script>
+            <link rel="stylesheet" href="styles.css">
+            <title>${objetoPostagem.tituloPostagem}</title>
+        </head>
+        <body>
+        <div class="postagemBlog"><id=\"${objetoPostagem.idPostagem}\"> 
+        <div id="tituloTexto"> <h1>${objetoPostagem.tituloPostagem}</h1> 
+        <img src="${objetoPostagem.imgPostagem}" alt="imagemPost${objetoPostagem.idPostagem}"> 
+        </div> <div id="autorTexto"> Por   ${objetoPostagem.autorPostagem} <br> </div> 
+        <div id="textoTexto"> <br> ${objetoPostagem.textoPostagem} </div></div>
+        <body>`)
 
+        // areaPost.innerHTML += `<div class="postagemBlog"><id=\"${objetoPostagem.idPostagem}\"> 
+        //                         <div id="tituloTexto"> <h1>${objetoPostagem.tituloPostagem}</h1> 
+        //                         <img src="${objetoPostagem.imgPostagem}" alt="imagemPost${objetoPostagem.idPostagem}"> 
+        //                         </div> <div id="autorTexto"> Por   ${objetoPostagem.autorPostagem} <br> </div> 
+        //                         <div id="textoTexto"> <br> ${objetoPostagem.textoPostagem} </div></div>`
+    } else if (objetoPostagem.imgPostagem == "") {
+        let opened = window.open();
+        opened.document.write(`<!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <meta http-equiv="X-UA-Compatible" content="ie=edge">
+            <script src="index.js" defer></script>
+            <link rel="stylesheet" href="styles.css">
+            <title>${objetoPostagem.tituloPostagem}</title>
+        </head>
+        <body>
+        <div class="postagemBlog"><id=\"${objetoPostagem.idPostagem}\"> 
+        <div id="tituloTexto"> <h1>${objetoPostagem.tituloPostagem}</h1>
+        </div> <div id="autorTexto"> Por   ${objetoPostagem.autorPostagem} <br> </div> 
+        <div id="textoTexto"> <br> ${objetoPostagem.textoPostagem} </div></div> 
+        <body>`)
+
+        // areaPost.innerHTML += `<div class="postagemBlog"><id=\"${objetoPostagem.idPostagem}\"> 
+        //                         <div id="tituloTexto"> <h1>${objetoPostagem.tituloPostagem}</h1>
+        //                         </div> <div id="autorTexto"> Por   ${objetoPostagem.autorPostagem} <br> </div> 
+        //                         <div id="textoTexto"> <br> ${objetoPostagem.textoPostagem} </div></div>`
     }
     
     

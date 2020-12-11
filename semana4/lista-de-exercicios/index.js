1d35752cce1cfe453314ad79d048284d18f5fc7b
Igor Chacon <igorchacon@yahoo.com.br>
2020-12-11 10:57:41 -0300

    exercicio 1 feito

---
+++ semana4/lista-de-exercicios/index.js
@@ -0,0 +1,36 @@
+// Exercícios de interpretação de código
+// 1
+/*
+O código é uma função que recebe um valor, pega esse valor numérico que é recebido em uma string, mas é transformado em number,
+depois retorna "R$" + o valor informado multiplicado pela cotação
+depois de executar a função, ele chama essa função (recebendo o valor 100) e guarda na variável "meuDinheiro"
+por fim, imprime essa variável.
+*/
+
+// 2
+/*
+Uma função que recebe dois parâmetros. Dentro dela, cria uma variável (valorAposInvestimento), cria um switch com 5 casos de tipos de investimento e, dependendo do case,
+multiplica o valor colocado como parâmetro e o multiplia por um número, o guardando na variável valorAposInvestimento. Por fim, retorna valorAposInvestimento.
+ao final, invoca a função duas vezes, uma para os marâmetros "Ações" e 150 e outra para "Tesouro direto" e 200, guardando o retorno em duas variáveis. Por fim, imprime no console essas variáveis.
+*/
+
+// 3
+/*
+Cria 3 arrays, um com números definidos e outro vazio.
+Depois há um for que preenche os dois arrays vazios, um com números impares e outro com os números pares
+do array inicialmente declarado com números.
+por fim, imprime 3 coisas:
+primeiro a quantidade de elementos do primeiro array
+depois o tamanho do array que recebeu os números pares do primeiro array
+depois o tamanho do array que recebem os números ímpares do primeiro array
+*/
+
+// 4
+/*
+cria um array com números chamado numeros
+cria uma variável numero1 que guarda o valor Infinity
+cria uma variável numero2 com o valor 0
+Em um for, guarda na variavel numero1 o menor valor do array numeros
+e o maior valor do array numeros na variável numero2
+por fim, imprime numero1 e numero2
+*/

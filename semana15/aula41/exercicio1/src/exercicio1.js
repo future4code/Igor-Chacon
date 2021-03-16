d48e7a14577f3a8f609a8d1f64acb4b7e5f9d3b0
Igor Chacon <igorchacon@yahoo.com.br>
2021-03-16 16:17:08 -0300

    semana15-aula41

---
+++ semana15/aula41/exercicio1/src/exercicio1.js
@@ -0,0 +1,6 @@
+var minhaString = "Igor Chacon";
+console.log(minhaString);
+//  a) minhaString = 10 caso eu tente colocar um number ele acusa erro antes mesmo de rodar
+var meuNumero = 10;
+console.log(meuNumero);
+// usamos o number | string para a variável também receber strings
---
+++ semana15/aula41/exercicio4/exercicio4.ts
@@ -0,0 +1,19 @@
+// a) Como você faria, já com a extensão 
+// instalada, para gerar um 
+// arquivo javascript a partir do  arquivo
+//  typescript com o código abaixo?
+
+// R.: digitaria o comando tsc [nome do arquivo]
+
+
+// b) E se este arquivo estivesse dentro de uma pasta chamada src. 
+// O processo seria diferente? Se sim, descreva as diferenças
+
+// R.: deveria fazer o comando src com o caminho relativo do arquivo
+
+// c) Existe alguma maneira de transpilar
+//  múltilplos arquivos de uma vez só? 
+// Caso conheça, explique como fazer.
+
+// R.: para isso precisamos do arquivo tsconfig.json
+// com ele usamos o comando tsc para vários arquivos de uma vez.

818ddc573b769fcb201e2da0ff539771cacc66b0
Igor Chacon <igorchacon@yahoo.com.br>
2021-03-20 19:27:36 -0300

    feito addUser e get all users

--- semana15/labebank/src/index.ts
+++ semana15/labebank/src/index.ts
@@ -17,31 +17,107 @@ const server = app.listen(process.env.PORT || 3003, () => {
 });
 
 type User = {
-    nome: string,
-    cpf: number,
-    dataDeNascimento: Date
+    name: string,
+    cpf: string,
+    bornDate: string,
+    saldo: number,
+    extrato: [
+        {
+            valor: number,
+            data: number,
+            descricao: string
+        }
+    ]
 }
 
-let users: User[] = []
+let users: User[] = [
+    {
+        name: "Igor",
+        cpf: "08291294488",
+        bornDate: "17/06/1989",
+        saldo: 0,
+        extrato: [
+            {
+                valor: 0,
+                data: 0,
+                descricao: "Saldo inicial"
+            }
+        ]
+    }
+]
+
+app.get('/users', (req: Request, res: Response)=> {
+    const result = users
+    res.status(200).send(result)
+})
 
 app.put('/createUser', (req: Request, res: Response) => {
     let errorCode: number = 400;
 
     try {
         const reqBody: User = {
-            nome: req.body.name,
+            name: req.body.name,
             cpf: req.body.cpf,
-            dataDeNascimento: req.body.date
+            bornDate: req.body.bornDate,
+            saldo: 0,
+            extrato: [
+                {
+                    valor: 0,
+                    data: Date.now(),
+                    descricao: 'Saldo inicial'
+                }
+            ]
         }
 
-        if(!reqBody.nome || 
+        if (!reqBody.name) {
+            errorCode = 422;
+            throw new Error("Invalid name.")
+        }
+
+        if (!reqBody.cpf) {
+            errorCode = 422;
+            throw new Error("Invalid cpf.")
+        }
+
+        if (!reqBody.bornDate) {
+            errorCode = 422;
+            throw new Error("Invalid born date.")
+        }
+
+        if (!reqBody.name || 
             !reqBody.cpf || 
-            !reqBody.dataDeNascimento
+            !reqBody.bornDate
             ){
              errorCode = 422;
              throw new Error("Please check the fields.")
          }
-    } catch (error) {
+
+
+         function calcularIdade(aniversario: string) {
+            var nascimento = aniversario.split("/");
+            var dataNascimento = new Date(parseInt(nascimento[2], 10),
+            parseInt(nascimento[1], 10) - 1,
+            parseInt(nascimento[0], 10));
         
+            var diferenca = Date.now() -  dataNascimento.getTime();
+            var idade = new Date(diferenca);
+        
+            return Math.abs(idade.getUTCFullYear() - 1970);
+        }
+
+        let dateBorn = reqBody.bornDate
+
+        let age: number = calcularIdade(dateBorn)
+
+         if (age < 18) {
+             errorCode = 422;
+             throw new Error("User must be 18 years older or more.")
+         }
+
+         users.push(reqBody);
+         res.status(201).send({message: "User created sucessfully!"})
+
+    } catch (error) {
+        res.status(errorCode).send({message: error.message});
     }
 })

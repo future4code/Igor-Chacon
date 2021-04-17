748ed6ef52fb0228cd7c245ab9cb614eae6b26b0
Igor Chacon <igorchacon@yahoo.com.br>
2021-04-17 13:02:17 -0300

    Projeto LaBook

---
+++ semana19/ProjetoLaBook/src/types.ts
@@ -0,0 +1,22 @@
+export type authenticationData = {
+    id: string
+}
+
+
+export type user = {
+    id: string,
+    name: string,
+    email: string,
+    password: string,
+};
+
+export type recipe = {
+    id: string,
+    title: string,
+    description: string,
+    createAt: string,
+    authorId: string
+};
+
+export const userTableName = 'cookenu_users';
+export const recipesTableName = 'cookenu_recipes';
